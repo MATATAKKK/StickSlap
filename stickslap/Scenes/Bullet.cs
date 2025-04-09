647ab0eee312b360decb243ec12b7ce1aa3d0ef4
Shaco__ <114386111+ShacoMHMD@users.noreply.github.com>
2025-04-09 16:01:24 +0200

    Affichage des balles et mouvements des balles

---
+++ stickslap/Scenes/Bullet.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class Bullet : Area2D
+{
+	[Export]
+	public int Speed = 400;
+
+	[Export]
+	public int Damage = 10;
+
+	private Vector2 _velocity;
+
+	public override void _Ready()
+	{
+		// Initialize the bullet's velocity
+		_velocity = new Vector2(Speed, 0).Rotated(Rotation);
+	}
+
+	public override void _Process(double delta)
+	{
+		// Move the bullet
+		Position += _velocity * (float)delta;
+	}
+
+	// private void OnAreaEntered(Area2D area)
+	// {
+		////Check if the bullet hit an enemy
+		// if (area is Enemy enemy)
+		// {
+			// enemy.TakeDamage(Damage);
+			// QueueFree(); // Remove the bullet after hitting
+		// }
+	// }
+	// private void OnBodyEntered(Node body)
+	// {
+		////Check if the bullet hit a wall or other object
+		// if (body is Wall)
+		// {
+			// QueueFree(); // Remove the bullet after hitting
+		// }
+	// }
+}
